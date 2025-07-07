8563a164645779c2129b496aad6cb5d270f9c956
EDOHWARES <edohemmanuel4real@gmail.com>
2025-07-07 05:37:53 +0100

    update strapi middleware

--- smk_cms/config/middlewares.ts
+++ smk_cms/config/middlewares.ts
@@ -2,7 +2,15 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://smk-super-client.onrender.com'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+      headers: '*',
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
